59b53812ccd116fc13571707476254041cbbeba3
Hugo <hugoo.ribeiro03@gmail.com>
2023-10-05 10:45:17 -0300

    versao validando token no post e adicionar id do usuario na tabela products

--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -15,13 +15,21 @@ export async function getProducts (req,res){
 export async function postProducts (req,res){
 	const {name, image, description, price} = req.body
 
+	const { authorization } = req.headers
+
+	const token = authorization?.replace("Bearer ","")
+
+	if(!token) return res.status(401).send("Envie o token na requisição")
+
     try{
+		const session = await db.query(`SELECT * FROM sessions WHERE token = '${token}';`)
+		if(session.rows.length === 0) return res.status(401).send("Envie um token valido")
+
 		const existProduct = await db.query(`SELECT name FROM products WHERE name = '${name}';`)
 		if(existProduct.rows.length > 0) return res.status(409).send("Produto já cadastrado")
 		
-
 		const newProduct = await db.query(
-			`INSERT INTO products (name, image, description, price) VALUES ('${name}', '${image}', '${description}', '${price}');`
+			`INSERT INTO products ("userId", name, image, description, price) VALUES ('${session.rows[0].userID}','${name}', '${image}', '${description}', '${price}');`
 		)
 
 		res.status(201).send(newProduct)
